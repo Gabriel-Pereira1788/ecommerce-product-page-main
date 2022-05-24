c43aef5e64acafbf4a73ec62a6724317f2ea9333
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2022-05-24 14:00:25 -0300

    Estilizando elementos do produto

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,15 @@
 const buttonNav=document.querySelector('.buttonNav')
 const buttonCloseNav=document.querySelector('.closeNav')
+const cartButton = document.querySelector('.cartButton')
+const cartShop=document.querySelector('.cart-shop')
 const header=document.querySelector('header')
 buttonNav.addEventListener('click',()=>{
 	header.classList.add('navOpen')
 })
 buttonCloseNav.addEventListener('click',()=>{
 	header.classList.remove('navOpen')
+})
+
+cartButton.addEventListener('click',()=>{
+	cartShop.classList.toggle('cart-desactive')
 })
